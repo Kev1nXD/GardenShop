1781a71a55c647afdcff95c2405f1bc883e34881
Kevin Korduner <kevinfdkord@gmail.com>
2024-05-22 20:49:10 +0300

    innit commit

--- ShopApi/models.py
+++ ShopApi/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductCriterion(models.Model):
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    criterion = models.ForeignKey(ProductCriterion, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=500)
+    last_name = models.CharField(max_length=500)
+    email = models.EmailField()
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=500)
+    state = models.CharField(max_length=500)
+    country = models.CharField(max_length=500)
+    postal_code = models.CharField(max_length=500)
+    phone_number = models.CharField(max_length=500)
+    
+    def __str__(self):
+        return self.id
--- ShopApi/views.py
+++ ShopApi/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
-# Create your views here.
+from .models import Category
+
+
+class MainPageView(TemplateView):
+    template_name = 'main_page.html'
+
+
+    def get_categories(self):
+        categories_subcategories = {category: category.subcategories.all() for category in Category.objects.all()}
+        return categories_subcategories
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories_subcategories"] = self.get_categories()
+        return context

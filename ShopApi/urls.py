842211736ab692c8cef8458fd28de774be754172
Kevin Korduner <kevinfdkord@gmail.com>
2024-05-27 02:18:51 +0300

    stash12

--- ShopApi/urls.py
+++ ShopApi/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, re_path
-from .views import MainPageView, ProductPageView, CategoryPageView, CartPageView
+from .views import MainPageView, ProductPageView, CategoryPageView, CartPageView, RecomendationsPageView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main_page'),
-    path('product_page/<int:pk>/', ProductPageView.as_view(), name='product_page'),
+    path(r'product_page/<int:pk>/', ProductPageView.as_view(), name='product_page'),
     re_path(r'category/(?P<category_id>\d+)/(?P<subcategory_id>\d+)/$', CategoryPageView.as_view(), name='category_page'),
     re_path(r'category/(?P<category_id>\d+)/$', CategoryPageView.as_view(), name='category_page'),
-    path('cart/', CartPageView.as_view(), name='cart_page'),
+    path(r'cart/', CartPageView.as_view(), name='cart_page'),
+    path(r'recomendations/', RecomendationsPageView.as_view(), name='recomendations_page'),
+    re_path('category/', CategoryPageView.as_view(), name='search_products'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ShopApi/views.py
+++ ShopApi/views.py
@@ -25,10 +25,13 @@ class CategoryPageView(ListView):
         queryset = Product.objects.all()
         category_id = self.kwargs.get('category_id')
         subcategory_id = self.kwargs.get('subcategory_id')
+        key_words = self.request.GET.get('key_words')
         if subcategory_id:
             queryset = queryset.filter(sub_category_id=subcategory_id)
         elif category_id:
             queryset = queryset.filter(sub_category__category_id=category_id)
+        if key_words:
+            queryset = queryset.filter(name__icontains=key_words)
 
         # Фильтрация по цвету
         color_id = self.request.GET.get('color')
@@ -54,11 +57,21 @@ class CategoryPageView(ListView):
         context = super().get_context_data(**kwargs)
         category_id = self.kwargs.get('category_id')
         subcategory_id = self.kwargs.get('subcategory_id')
+        key_words = self.request.GET.get('key_words')
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
+        color_id = self.request.GET.get('color')
+        manufacturer_id = self.request.GET.get('manufacturer')
+        context['min_price'] = min_price
+        context['max_price'] = max_price
+        context['color'] = Color.objects.get(pk=color_id) if color_id else None
+        context['manufacturer'] = Manufacturer.objects.get(pk=manufacturer_id) if manufacturer_id else None
         if subcategory_id:
             context['subcategory'] = SubCategory.objects.get(pk=subcategory_id)
         elif category_id:
             context['category'] = Category.objects.get(pk=category_id)
-
+        if key_words:
+            context['key_words'] = key_words
         # Передача всех доступных цветов и производителей в контекст
         context['colors'] = Color.objects.all()
         context['manufacturers'] = Manufacturer.objects.all()
@@ -67,3 +80,28 @@ class CategoryPageView(ListView):
 
 class CartPageView(TemplateView):
     template_name = 'cart.html'
+
+class RecomendationsPageView(View):
+
+    def recommendation_algorithm(self, local_storage):
+        reviewed_products = local_storage.get('reviewed_products', [])
+
+
+    @staticmethod
+    def create_product_html(products):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        print(data)
+        recommended_products = self.recommendation_algorithm(data)
+        products_html = self.create_product_html(recommended_products)
+        return JsonResponse({'products': products_html})
+
+class SaveReview(View):
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        Product.objects.get(pk=pk).reviews.create(
+            review=data['review'])
+        Product.get(pk=pk).rate = rating + Product.get(pk=pk).rate if Product.get(pk=pk).rate else rating
